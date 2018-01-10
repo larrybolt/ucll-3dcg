a9c5c70565886d48bcc70966ddeb27435896dd53
Bram <bram.vanasschodt@student.ucll.be>
2018-01-10 16:36:32 +0100

    error when rendering scene with ppm

--- raytracer/raytracer/imaging/ppm-format.cpp
+++ raytracer/raytracer/imaging/ppm-format.cpp
@@ -7,18 +7,20 @@ namespace imaging
 		uint32_t width = bitmap.width();
 		uint32_t height = bitmap.height();
 
-		out.write("P3 ", 3);
-		out.write(reinterpret_cast<char*>(&width), sizeof(uint32_t));
-		out.write(reinterpret_cast<char*>(&height), sizeof(uint32_t));
-		out.write("255 ", 4);
+		out << "P3" << "\n"
+			<< width << " "
+			<< height << "\n"
+			<< "255" << "\n"
+			;
 
 		for (unsigned i = 0; i != width; i++)
 		{
 			for (unsigned j = 0; i != height; j++)
 			{
 				Color color(bitmap[Position2D(i, j)]);
-				out.write(reinterpret_cast<char*>(&color), sizeof(color) + 1);
+				out << color << " ";
 			}
+			out << "\n";
 		}
 	}
 }
