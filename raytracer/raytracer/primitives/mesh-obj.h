cbd974d25d4e5556c09d67f7201e78948ba145cc
Bram <bram.vanasschodt@student.ucll.be>
2018-01-17 14:22:45 +0100

    infinite loop when reading .obj file

--- raytracer/raytracer/primitives/mesh-obj.h
+++ raytracer/raytracer/primitives/mesh-obj.h
@@ -6,17 +6,6 @@ namespace raytracer
 {
 	namespace primitives
 	{
-			class ObjMesh : public raytracer::primitives::_private_::PrimitiveImplementation
-			{
-			public:
-				std::vector<Primitive> m_triangles;
-				ObjMesh(const std::string& path) : m_triangles(read_mesh(path))
-				{ }
-				std::vector<std::shared_ptr<Hit>> find_all_hits(const math::Ray& ray) const override;
-				math::Box bounding_box() const override;
-				std::vector<Primitive> read_mesh(const std::string&) const;
-			};
-		std::shared_ptr<ObjMesh> obj_mesh(std::string path);
-		Primitive mesh(std::string path);
+		Primitive mesh(const std::string&);
 	}
 }
