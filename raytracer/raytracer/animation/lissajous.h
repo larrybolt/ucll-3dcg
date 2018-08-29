dcd1661cc6e4d1cdaaf2813242ff99e3a25a0f1a
Michiel <michiel.vh@hotmail.com>
2018-08-29 09:23:54 +0200

    fix

--- raytracer/raytracer/animation/lissajous.h
+++ raytracer/raytracer/animation/lissajous.h
@@ -1,8 +1,8 @@
-#pragma once
-#include "animation.h"
-using namespace math;
-
-namespace animation {
-	Animation<Point3D> lissajous(const int, const int, const int, const int, const int, const int, const Angle, const Angle, const Angle, const Duration&);
-
-}
+//#pragma once
+//#include "animation.h"
+//using namespace math;
+//
+//namespace animation {
+//	Animation<math::Point3D> lissajous(const int, const int, const int, const int, const int, const int, const Angle, const Angle, const Angle, const Duration&);
+//
+//}
